c960e06d81f5f1420648797c069cab1979497701
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-03-31 22:02:45 +0900

    Initial Commit

---
+++ WeeklyCalendar/WeeklyCalendarApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeeklyCalendarApp.swift
+//  WeeklyCalendar
+//
+//  Created by Hiroaki-Hirabayashi on 2022/03/31.
+//
+
+import SwiftUI
+
+@main
+struct WeeklyCalendarApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
